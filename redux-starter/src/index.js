6be2b66eab62676c9bc03150a77d49a188613faf
Najim Uddin <najim7304@gmail.com>
2023-02-17 23:22:47 +0600

    Single add and remove reducer

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,5 +1,6 @@
 import store from "./store";
 store.dispatch({ type: "ADD_TASK", payload: { task: "This is task 01" } });
 store.dispatch({ type: "ADD_TASK", payload: { task: "This is task 02" } });
+store.dispatch({ type: "ADD_TASK", payload: { task: "This is task 03" } });
 store.dispatch({ type: "REMOVE_TASK", payload: { id: 1 } });
 console.log(store.getState());
