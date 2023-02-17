5163fbf12468803f134e7c26577e2c8d96cbfbce
Najim Uddin <najim7304@gmail.com>
2023-02-17 20:17:06 +0600

    Immer

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,13 +1,16 @@
-// spread operator
+import { produce } from "immer";
 const employee = {
     name: "Herly",
     age: 34,
     company: { country: "Canada", city: "Toronto" },
-}
-const newEmployee = {
-    ...employee,
-    name: "Martin",
-    company: { ...employee.company, city: 'Calgary' }
-}
-console.log(employee)
-console.log(newEmployee)
+};
+// const newEmployee = {
+//     ...employee,
+//     name: "Martin",
+//     company: { ...employee.company, city: 'Calgary' }
+// }
+const newEmployee = produce(employee, (draftState) => {
+    (draftState.name = "Najim"), (draftState.company.city = "Dhaka");
+});
+console.log(employee);
+console.log(newEmployee);
