010c7faee8f72f939f74a5df21be2b95a4469ba0
Najim Uddin <najim7304@gmail.com>
2023-02-17 21:31:00 +0600

    immutability

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,16 +1,20 @@
-import { produce } from "immer";
-const employee = {
-    name: "Herly",
-    age: 34,
-    company: { country: "Canada", city: "Toronto" },
-};
-// const newEmployee = {
-//     ...employee,
-//     name: "Martin",
-//     company: { ...employee.company, city: 'Calgary' }
-// }
-const newEmployee = produce(employee, (draftState) => {
-    (draftState.name = "Najim"), (draftState.company.city = "Dhaka");
-});
-console.log(employee);
-console.log(newEmployee);
+// IMMUTABILITY 
+const numbers = [10, 20, 30, 40];
+console.log(numbers)
+// ADDING ITEMS
+const addNumbers = [50, ...numbers]
+console.log(addNumbers)
+
+// REPLACE 50 IN PLACE OF 30 
+
+const index = numbers.indexOf(30);
+const addNumbers2 = [...numbers.slice(0, index), 50, ...numbers.slice(index)]
+console.log(addNumbers2)
+
+// UPDATING ITEMS 
+const updated = numbers.map(a => a === 40 ? 80 : a);
+console.log(updated)
+
+// REMOVING ITEMS 
+const removed = numbers.filter(a => a != 30)
+console.log(removed)
