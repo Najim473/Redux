8a21d77ee749397bf16187a98ce7984cd905c3ec
Najim Uddin <najim7304@gmail.com>
2023-02-17 20:00:16 +0600

    curry

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -3,8 +3,17 @@ let username = "Herly";
 let message = "Hello " + username.trim() + "Good Morning";
 console.log(message);
 const trim = (name) => name.trim();
-const generateMessage = (name) => `Hello ${name}, Good Morning!`;
+const generateMessage = (message) => (name) => `Hello ${name}, ${message}!`;
 const convertToUpper = (name) => name.toUpperCase();
-const newFunc = pipe(trim, convertToUpper, generateMessage)
+const newFunc = pipe(trim, convertToUpper, generateMessage('Have a nice day'))
 const result = newFunc(username)
 console.log(result);
+
+
+function multi(a) {
+    return function (b) {
+        return a * b
+    }
+}
+let result1 = multi(1)(3)
+console.log(result1)
