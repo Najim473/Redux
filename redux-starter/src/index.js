cddcf6e757042de86fe4b127d1083dfb2124b939
Najim Uddin <najim7304@gmail.com>
2023-02-17 20:13:07 +0600

    spread operator

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,19 +1,13 @@
-import { pipe } from 'lodash/fp';
-let username = "Herly";
-let message = "Hello " + username.trim() + "Good Morning";
-console.log(message);
-const trim = (name) => name.trim();
-const generateMessage = (message) => (name) => `Hello ${name}, ${message}!`;
-const convertToUpper = (name) => name.toUpperCase();
-const newFunc = pipe(trim, convertToUpper, generateMessage('Have a nice day'))
-const result = newFunc(username)
-console.log(result);
-
-
-function multi(a) {
-    return function (b) {
-        return a * b
-    }
+// spread operator
+const employee = {
+    name: "Herly",
+    age: 34,
+    company: { country: "Canada", city: "Toronto" },
 }
-let result1 = multi(1)(3)
-console.log(result1)
+const newEmployee = {
+    ...employee,
+    name: "Martin",
+    company: { ...employee.company, city: 'Calgary' }
+}
+console.log(employee)
+console.log(newEmployee)
