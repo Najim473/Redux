e7c8f9c285fb7496402ebbbf8ae3b56064e9ef0f
Najim Uddin <najim7304@gmail.com>
2023-02-17 19:46:57 +0600

    lodash pipe

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,10 @@
-console.log("Redux Starter Project");
+import { compose, pipe } from 'lodash/fp';
+let username = "Herly";
+let message = "Hello " + username.trim() + "Good Morning";
+console.log(message);
+const trim = (name) => name.trim();
+const generateMessage = (name) => `Hello ${name}, Good Morning!`;
+const convertToUpper = (name) => name.toUpperCase();
+const newFunc = pipe(trim, convertToUpper, generateMessage)
+const result = newFunc(username)
+console.log(result);
