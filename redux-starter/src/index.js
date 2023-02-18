f04d0c1459eb9f13a0b1720893d1da4662eff437
Najim Uddin <najim7304@gmail.com>
2023-02-18 14:37:24 +0600

    createAcitons using redux-toolkit

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,13 +1,12 @@
 import store from "./store/configureStore";
 import { addTask, removeTask, completedTask } from "./store/tasks.js";
-const unSubscribe = store.subscribe(() => {
-    console.log("Updated", store.getState());
-});
-store.dispatch(addTask("Task 01"));
-store.dispatch(addTask("Task 02"));
+// const unSubscribe = store.subscribe(() => {
+//     console.log("Updated", store.getState());
+// });
+store.dispatch(addTask({ task: "Task 01" }));
+store.dispatch(addTask({ task: "Task 02" }));
 console.log(store.getState());
-
-unSubscribe()
-store.dispatch(completedTask(2))
-store.dispatch(removeTask(1));
+// unSubscribe();
+store.dispatch(completedTask({ id: 2 }));
+store.dispatch(removeTask({ id: 1 }));
 console.log(store.getState());
--- redux-starter/src/store/tasks.js
+++ redux-starter/src/store/tasks.js
@@ -1,28 +1,25 @@
 import { createAction } from "@reduxjs/toolkit";
-const test = createAction("ADD_TASK");
-console.log(test({ task: 'Task create actions' }))
-// Action Types
-const ADD_TASK = "ADD_TASK";
-const REMOVE_TASK = "REMOVE_TASK";
-const COMPLETED_TASK = "COMPLETED_TASK";
 // ACTIONS
-export const addTask = (task) => {
-    return { type: ADD_TASK, payload: { task: task } };
-};
+export const addTask = createAction("ADD_TASK");
+export const removeTask = createAction("REMOVE_TASK");
+export const completedTask = createAction("COMPLETED_TASK");
+console.log(completedTask.type);
+// export const addTask = (task) => {
+//     return { type: ADD_TASK, payload: { task: task } };
+// };
+// export const removeTask = (id) => {
+//     return { type: REMOVE_TASK, payload: { id: id } };
+// };
 
-export const removeTask = (id) => {
-    return { type: REMOVE_TASK, payload: { id: id } };
-};
-
-export const completedTask = (id) => {
-    return { type: COMPLETED_TASK, payload: { id: id } };
-};
+// export const completedTask = (id) => {
+//     return { type: COMPLETED_TASK, payload: { id: id } };
+// };
 
 // REDUCER
 let id = 0;
 export default function reducer(state = [], action) {
     switch (action.type) {
-        case ADD_TASK: {
+        case addTask.type: {
             return [
                 ...state,
                 {
@@ -32,10 +29,10 @@ export default function reducer(state = [], action) {
                 },
             ];
         }
-        case REMOVE_TASK: {
+        case removeTask.type: {
             return state.filter((task) => task.id !== action.payload.id);
         }
-        case COMPLETED_TASK: {
+        case completedTask.type: {
             return state.map((task) =>
                 task.id === action.payload.id
                     ? {
