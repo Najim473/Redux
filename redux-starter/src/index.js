8b90db48b1aa6064da9f27a083045cb3c34f772c
Najim Uddin <najim7304@gmail.com>
2023-02-18 21:40:48 +0600

    middlware error

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -5,13 +5,14 @@ import { addEmployee } from "./store/employees";
 //     console.log("Updated", store.getState());
 // });
 // console.log(addEmployee())
-store.dispatch(addTask({ task: "Task 01" }));
-store.dispatch(addTask({ task: "Task 02" }));
+// store.dispatch(addTask({ task: "Task 01" }));
+// store.dispatch(addTask({ task: "Task 02" }));
 // console.log(store.getState());
 // unSubscribe();
-store.dispatch(completedTask({ id: 2 }));
-store.dispatch(removeTask({ id: 1 }));
+// store.dispatch(completedTask({ id: 2 }));
+// store.dispatch(removeTask({ id: 1 }));
 // console.log(store.getState());
 ////////// EMPLOYEE ////////////
-store.dispatch(addEmployee({ employee: "Najim" }))
+// store.dispatch(addEmployee({ employee: "Najim" }))
+store.dispatch(({ type: "SHOW_ERROR", payload: { error: "user not found" } }))
 // console.log(store.getState)
--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -3,6 +3,7 @@ import log from "../middleware/log";
 import employeeReducer from "./employees";
 import tasksReducer from "./tasks";
 import logger from 'redux-logger'
+import error from "../middleware/error";
 const store = configureStore({
     reducer: {
         tasks: employeeReducer,
@@ -11,7 +12,7 @@ const store = configureStore({
     // WE CAN ALSO APPLY THIS WAY 
     // middleware: [log]
     // ALTERNATIVE WAY 
-    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), log]
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger, error]
 });
 
 export default store;
