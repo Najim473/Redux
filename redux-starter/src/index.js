1fbea990649275855cd2edc64a8fae46f97e423d
Najim Uddin <najim7304@gmail.com>
2023-02-18 11:40:28 +0600

    subscribe & unsubscribe

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,7 +1,11 @@
 import store from "./store";
 import { addTask, removeTask } from "./action";
+const unSubscribe = store.subscribe(() => {
+    console.log("Updated", store.getState());
+});
 store.dispatch(addTask("Task 01"));
-store.dispatch(addTask("Task 02"));
-store.dispatch(addTask("Task 03"));
+console.log(store.getState());
 store.dispatch(removeTask(1));
+unSubscribe()
+store.dispatch(removeTask(2));
 console.log(store.getState());
