6cb95843cff8bd79f052851f5d5b183a56cf49a5
Najim Uddin <najim7304@gmail.com>
2023-02-20 13:09:12 +0600

    creating general api action

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,12 +1,14 @@
-import store from "./store/configureStore"
-import { getTasks } from "./store/tasks"
-console.log(getTasks())
-store.dispatch({
-    type: "apiRequest",
-    payload: {
+import store from "./store/configureStore";
+import { getTasks } from "./store/tasks";
+import { apiCallBegan } from "./store/api";
+// console.log(getTasks());
+
+store.dispatch(
+    apiCallBegan({
         url: "/tasks",
         onStart: "tasks/apiRequested",
         onSuccess: "tasks/getTasks",
-        onError: "tasks/apiRequestFailed"
-    }
-})
+        onError: "tasks/apiRequestFailed",
+
+    })
+);
--- redux-starter/src/store/middleware/api.js
+++ redux-starter/src/store/middleware/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
+import { apiCallBegan } from "../api";
 const api =
     ({ dispatch }) =>
         (next) =>
             async (action) => {
-                if (action.type !== "apiRequest") return next(action);
-
+                if (action.type !== apiCallBegan.type) return next(action);
                 const { url, method, data, onStart, onSuccess, onError } = action.payload;
                 if (onStart) dispatch({ type: onStart });
 
